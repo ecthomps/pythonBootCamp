1bdd8ed83104172eab71a9a896a8e69ffff46a63
Eranus Thompson <ecthomps@umich.edu>
2018-05-12 18:42:10 -0400

    added variables.py

---
+++ variables.py
@@ -0,0 +1,18 @@
+x = 10;
+y = 10;
+print(x + y);
+
+newline = "hi\nthere"
+print(newline);
+
+mountains = "/\\/\\/\\"
+print(mountains)
+
+str_one = "ice"
+str_one += " cream"
+print(str_one)
+
+print(f"I have {x} " + str_one + "s")
+
+name = "Chuck"
+print(name[2])
