3415bd17ae0f58e537bfbbf6d0ee3a50fd157f1d
Eranus <ecthomps@umich.edu>
2018-05-16 16:25:20 -0400

    added screamer

---
+++ screamer.py
@@ -0,0 +1,7 @@
+num = int(input("How many times do I have to tell you? "))
+
+# for x in range(num):
+#     print("CLEAN UP YOUR ROOM!")
+
+for x in range(num):
+    print(f"time is {x+1}: CLEAN UP YOUR ROOM!")
