a6f1477ea67330132e84cab258069e703d50044f
Eranus <ecthomps@umich.edu>
2018-05-16 16:08:14 -0400

    added comp2Player RPS

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -1,25 +1,57 @@
+import random
+items = ["rock", "paper", "scissors"]
+
 print("...rock...\n...paper...\n...scissors...")
 
 p1Choice = input("Enter Player 1's choice: ")
-p2Choice = input("Enter Player 2's choice: ")
+# p2Choice = input("Enter Player 2's choice: ")
+compChoice = random.choice(items)
+rand_num = random.randint(0,2)
 
-if p1Choice == p2Choice: 
+if rand_num == 0:
+    comp = "rock"
+elif rand_num == 1:
+    comp = "paper"
+else:
+    comp = "scissors"
+
+print("The computer played: " + comp)
+
+if p1Choice == comp: 
     print("We hava a tie")
 elif p1Choice == "rock":
-    if p2Choice == "paper":
-        print("player2 wins")
-    elif p2Choice == "scissors":
+    if comp == "paper":
+        print("computer wins")
+    elif comp == "scissors":
         print("player1 wins")
 elif p1Choice == "paper":
-    if p2Choice == "rock":
+    if comp == "rock":
         print("player1 wins")
-    elif p2Choice == "scissors":
-        print("player2 wins")
+    elif comp == "scissors":
+        print("computer wins")
 elif p1Choice == "scissors":
-    if p2Choice == "paper":
+    if comp == "paper":
         print("player1 wins")
-    elif p2Choice == "rock":
-        print("player2 wins")
+    elif comp == "rock":
+        print("computer wins")
+
+# if p1Choice == p2Choice: 
+#     print("We hava a tie")
+# elif p1Choice == "rock":
+#     if p2Choice == "paper":
+#         print("player2 wins")
+#     elif p2Choice == "scissors":
+#         print("player1 wins")
+# elif p1Choice == "paper":
+#     if p2Choice == "rock":
+#         print("player1 wins")
+#     elif p2Choice == "scissors":
+#         print("player2 wins")
+# elif p1Choice == "scissors":
+#     if p2Choice == "paper":
+#         print("player1 wins")
+#     elif p2Choice == "rock":
+#         print("player2 wins")
 
 # if p1Choice == "rock" and p2Choice == "paper":
 #     print("player2 wins")
