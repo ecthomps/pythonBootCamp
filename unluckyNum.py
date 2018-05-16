0efc1e6050babc571e6a02609518efd60882cdc5
Eranus <ecthomps@umich.edu>
2018-05-16 16:43:15 -0400

    added unluckyNum

---
+++ unluckyNum.py
@@ -0,0 +1,16 @@
+# for num in range(1,21):
+#     if(num == 4 or num == 13):
+#         print(f"{num} is UNLUCKY!")
+#     elif(num%2 == 0):
+#         print(f"{num} is even")
+#     else:
+#         print(f"{num} is odd")
+
+for num in range(1,21):
+    if(num == 4 or num == 13):
+        state = "UNLUCKY!"
+    elif(num%2 == 0):
+        state = "even"
+    else:
+        state = "odd"
+    print(f"{num} is " + state)
