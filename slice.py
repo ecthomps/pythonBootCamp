6340d6bff334227c31f6aae9612dbd69908ba543
Christian Thompson <ecthomps@umich.edu>
2018-07-24 23:42:34 -0700

    added slice

---
+++ slice.py
@@ -0,0 +1,14 @@
+colors = ["red", "green", "orange", "yellow", "green", "blue", "violet"]
+
+print(colors[3:])
+
+print(colors[:3])
+
+print(colors[2:5])
+
+colors2 = colors[:]
+
+print(colors2[:])
+
+# step is number of count at a time
+print(colors[1::2])
