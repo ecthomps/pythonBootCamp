bec5319d939cd715725db322b975e50d2a973b5d
Christian Thompson <ecthomps@umich.edu>
2018-07-24 23:48:40 -0700

    added swap

---
+++ swap.py
@@ -0,0 +1,10 @@
+# swapping values
+
+names = ['James', 'Mary']
+
+names[0], names[1] = names[1], names[0]
+
+for x in range(len(names)):
+    print(names[x])
+
+# used swap for: shuffling, sorting, algorithm
