1d0bcf81acf259a2e50b8135e3d72f0ae72b3b7b
Eranus Thompson <ecthomps@umich.edu>
2018-08-25 02:47:31 -0400

    added LC with comprehension

--- listComprehension.py
+++ listComprehension.py
@@ -18,3 +18,36 @@ print(res)
 number = [1,2,3,4,5]
 res = [str(num) for num in number]
 print(res)
+
+#LC with conditional logic
+numbers = [1, 2, 3, 4, 5, 6]
+
+evens = [num for num in numbers if num % 2 == 0]
+print(f'even numbers:  {evens}')
+
+odd = [num for num in numbers if num % 2 == 1]
+print(f'odd numbers: {odd}')
+
+res = [num*2 if num % 2 == 0 else num/2 for num in numbers]
+print(f'results: {res}')
+
+with_vowels = "This is so much fun!"
+res = ''.join(char for char in with_vowels if char not in "aeiou")
+print(res)
+
+
+#create a list containing first letter of each name
+person = ['Ellie', 'Tim', 'Matt']
+answer = [char[0] for char in person]
+print(answer)
+
+#create a list that is the intersection of two list
+l1 = [1,2,3,4]
+l2 = [3,4,5,6]
+
+res = [instc for instc in l1 if instc in l2]
+print(res)
+
+#create a list with each word reversed and in lower case
+answer = [name.lower()[::-1] for name in person]
+print(answer)
