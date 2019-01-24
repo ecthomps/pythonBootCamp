c91e37abb401ff6a8197845a52e6ec52b91594e2
Eranus Thompson <ecthomps@umich.edu>
2019-01-23 19:57:22 -0600

    added sockMerchant

--- hackerrank/sockMerchant.py
+++ hackerrank/sockMerchant.py
@@ -2,7 +2,7 @@ arr = [10,20,20,10,10,30,50,10,20]
 
 def printNumOfPairs(lst):
     count = 0
-    k = set(lst)
+    k = set(lst) 
     for i in k:
         if lst.count(i)//2>=1:
             count += (lst.count(i)//2)
